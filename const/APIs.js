58f66f444003145803a1f1f7b1f701428a662771
ctofastech <cto@fastechsystem.co>
2023-09-05 21:57:33 +0500

    write sh file and secure the folders

--- const/APIs.js
+++ const/APIs.js
@@ -1,7 +1,6 @@
-export const BASE_URL_= 'http://localhost:5000'
-
+export const BASE_URL_BACKEND = process.env.BASE_URL_BACKEND
 export const APIs = {
-    GET_ALL_CSV: `${BASE_URL_}/get-all-csv`,
-    UPLOAD_CSV: `${BASE_URL_}/upload-csv`,
-    PROCESS_MENU: `${BASE_URL_}/process-menu`,
+    GET_ALL_CSV: `${BASE_URL_BACKEND}/get-all-csv`,
+    UPLOAD_CSV: `${BASE_URL_BACKEND}/upload-csv`,
+    PROCESS_MENU: `${BASE_URL_BACKEND}/process-menu`,
 }
---
+++ start.sh
@@ -0,0 +1,4 @@
+ls
+npm install
+export BASE_URL_BACKEND="http://16.171.171.183:5002"
+npm run dev -p 3004
